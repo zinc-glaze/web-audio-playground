5ab9721b7daac3fbedd9e6fed5397dee1d0c6548
Zinc Glaze <zekegraves@gmail.com>
2019-03-20 18:30:17 -0400

    created Sound class

--- web-audio-test-1.js
+++ web-audio-test-1.js
@@ -4,22 +4,29 @@ var context = new (window.AudioContext || window.webkitAudioContext)();
 
 //OscillatorNode.type = 'sine'|'square'|'triangle'|'sawtooth';
 
-var oscillator = context.createOscillator();
+var osc01 = context.createOscillator();
+var osc02 = context.createOscillator();
 var gain = context.createGain();
 var now = context.currentTime;
 
-oscillator.type = 'sine';
-oscillator.frequency.value = 220;
-oscillator.connect(gain);
+osc01.type = 'sine';
+osc01.frequency.value = 440;
+osc01.connect(gain);
+osc01.start(now);
+osc01.stop(now + 5);
+
+osc02.type = 'square';
+osc02.frequency.value = 110;
+osc02.connect(gain);
+osc02.start(now + 2);
+osc02.stop(now + 5);
+
 gain.connect(context.destination);
-
-
 gain.gain.setValueAtTime(0.5, now);
-// gain.gain.exponentialRampToValueAtTime(0.001, now + 0.5);
-oscillator.start(now);
-oscillator.stop(now + 3);
+gain.gain.exponentialRampToValueAtTime(0.001, now + 5);
 
 
+//button click handler
 document.querySelector('button').addEventListener('click', function() {
   context.resume().then(() => {
     console.log('Playback resumed successfully');
